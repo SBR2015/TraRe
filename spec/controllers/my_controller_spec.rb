39eef13164b1e29e2f835156ef4d0b572fde1c5a
pyar6329 <pyar6329@gmail.com>
2015-04-15 01:04:30 +0900

    #21 rspec test add little

--- spec/controllers/my_controller_spec.rb
+++ spec/controllers/my_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe MyController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  pending "add some examples to (or delete) #{__FILE__}"
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -84,4 +84,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+  # devise settings
+  config.include Devise::TestHelpers, type: :controller
 end
