16462d9e1122d16375a32180e89ba75cbd577940
pyar6329 <pyar6329@gmail.com>
2015-04-16 21:51:13 +0900

    #33 add model relationship

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,2 +1,19 @@
 class Language < ActiveRecord::Base
+  # model relationship
+  has_many :native_users,
+           class_name: 'User',
+           foreign_key: 'native_language_id'
+
+  has_many :best_users,
+           class_name: 'User',
+           foreign_key: 'best_language_id'
+
+  has_many :written_resumes,
+           class_name: 'Resume',
+           foreign_key: 'language_id'
+
+  has_many :wanted_resumes,
+           class_name: 'Resume',
+           foreign_key: 'wanted_language_id'
+
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,2 +1,19 @@
 class Resume < ActiveRecord::Base
+  # model relationship
+  belongs_to :written_language,
+             class_name: 'Language',
+             foreign_key: 'language_id'
+
+  belongs_to :wanted_language,
+             class_name: 'Language',
+             foreign_key: 'wanted_language_id'
+
+  belongs_to :owner,
+             class_name: 'User',
+             foreign_key: 'owner_id'
+
+  belongs_to :translator,
+             class_name: 'User',
+             foreign_key: 'translator_id'
+
 end
