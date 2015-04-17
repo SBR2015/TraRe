8b32965169c57f7033f768a0bd63b865a6d558db
Olivia-tou <olivia_tou@yahoo.co.jp>
2015-04-17 14:38:18 +0000

    resume ???

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -9,7 +9,7 @@ class ResumesController < ApplicationController
   end
 
   def new
-    @resume = Resume.new
+    @resumes = Resume.new
     render :json => {:form => render_to_string(:partial => 'form')}
   end
 
@@ -17,34 +17,34 @@ class ResumesController < ApplicationController
   end
 
   def create
-    @resume = Resume.new(resume_params)
+    @resumes = Resume.new(resume_params)
 
     respond_to do |format|
-      if @resume.save
-        format.html { redirect_to @resume, notice: 'Resume was successfully created.' }
-        format.json { render :show, status: :created, location: @resume }
+      if @resumes.save
+        format.html { redirect_to @resumes, notice: 'Resume was successfully created.' }
+        format.json { render :show, status: :created, location: @resumes }
       else
         format.html { render :new }
-        format.json { render json: @resume.errors, status: :unprocessable_entity }
+        format.json { render json: @resumes.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def update
     respond_to do |format|
-      if @resume.update(resume_params)
-        format.html { redirect_to @resume, notice: 'Resume was successfully updated.' }
-        format.json { render :show, status: :ok, location: @resume }
+      if @resumes.update(resume_params)
+        format.html { redirect_to @resumes, notice: 'Resume was successfully updated.' }
+        format.json { render :show, status: :ok, location: @resumes }
       else
         format.html { render :edit }
-        format.json { render json: @resume.errors, status: :unprocessable_entity }
+        format.json { render json: @resumes.errors, status: :unprocessable_entity }
       end
     end
   end
 
 
   def destroy
-    @resume.destroy
+    @resumes.destroy
     respond_to do |format|
       format.html { redirect_to resumes_url, notice: 'Resume was successfully destroyed.' }
       format.json { head :no_content }
@@ -54,7 +54,7 @@ class ResumesController < ApplicationController
   private
 
     def set_resume
-      @resume = Resume.find(params[:id])
+      @resumes = Resume.find(params[:id])
     end
 
     def resume_params
