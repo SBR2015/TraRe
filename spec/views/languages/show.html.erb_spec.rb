19c4a666fcd7204fb8dd9fbd6b50c92747df58a6
pyar6329 <pyar6329@gmail.com>
2015-04-22 20:24:23 +0900

    #37 language scaffold

---
+++ spec/views/languages/show.html.erb_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "languages/show", type: :view do
+  # before(:each) do
+  #   @language = assign(:language, Language.create!(
+  #     :code => "Code"
+  #   ))
+  # end
+  #
+  # it "renders attributes in <p>" do
+  #   render
+  #   expect(rendered).to match(/Code/)
+  # end
+end
