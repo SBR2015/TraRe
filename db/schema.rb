c479b401a22a4e3c04f001a7678e8e3887c9c734
pyar6329 <pyar6329@gmail.com>
2015-04-22 21:15:16 +0900

    #37 migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416113215) do
+ActiveRecord::Schema.define(version: 20150422113215) do
 
   create_table "languages", force: :cascade do |t|
     t.string   "code",       limit: 2, null: false
