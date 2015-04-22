ac67c8649aed9dd8f2ad696f388777be22d81045
pyar6329 <pyar6329@gmail.com>
2015-04-22 21:22:22 +0900

    #37 migration fix

--- db/migrate/20150422104021_create_resumes.rb
+++ db/migrate/20150422104021_create_resumes.rb
@@ -2,7 +2,7 @@ class CreateResumes < ActiveRecord::Migration
   def change
     create_table :resumes do |t|
       t.text :sentence, null: false, default: ""
-      t.boolean :is_translation, null: false, default: 0
+      t.boolean :is_translation, null: false, default: false
       t.integer :owner_id
       t.integer :translator_id
       t.references :language, index: true, foreign_key: true
