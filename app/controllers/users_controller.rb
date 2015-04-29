06c8075a269bb52bcecc9d89e980972c8da5f02f
Yusuke Ohashi <yusuke@junkpiano.me>
2015-04-29 17:16:16 +0900

    prepared user's page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
+  before_action :setuser
 
   def show
+  end
+
+  private
+
+  def setuser
+    @user = User.find_by(username: params[:username])
+    render text: 'the user doesn\'t exist.', status: 404 if @user.nil?
   end
 end
