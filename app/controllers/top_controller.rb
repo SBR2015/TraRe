6b5276320276a3ca03d49b089855a390a4379452
Yusuke Ohashi <yusuke@junkpiano.me>
2015-04-29 23:34:33 +0900

    redirect to userpage when users are signed in.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    redirect_to user_path(username: current_user.username) if user_signed_in?
   end
 end
--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe TopController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
+  pending "add some examples to (or delete) #{__FILE__}"
+  # describe "GET #index" do
+  #   it "returns http success" do
+  #     get :index
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
