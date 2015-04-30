af1057e80703bf902319c7bec32a75267682b76a
yuchan <yusuke@junkpiano.me>
2015-04-30 14:04:29 +0900

    add new resume link.

--- spec/requests/languages_spec.rb
+++ spec/requests/languages_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Languages", type: :request do
-  describe "GET /languages" do
-    it "works! (now write some real specs)" do
-      get languages_path
-      expect(response).to have_http_status(200)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  # describe "GET /languages" do
+  #   it "works! (now write some real specs)" do
+  #     get languages_path
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
 end
