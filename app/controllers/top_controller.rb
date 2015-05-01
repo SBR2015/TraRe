c31faa1ce1f5163a9011bd5f8df557585842189f
shinjukujohnny <a13z3hk@aiit.ac.jp>
2015-05-01 09:29:41 +0900

    modify top modules

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,7 @@
 class TopController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
+    @resumes = params[:language_id].present? ? Resume.where(language_id: params[:language_id]) : Resume.all
   end
 end
