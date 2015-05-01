7fb9a1b8b05ba3cf1df625855e932bcabc41084a
shinjukujohnny <a13z3hk@aiit.ac.jp>
2015-05-01 09:59:20 +0900

    add comments

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,6 +2,7 @@ class TopController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    # owner_id != 自分のid のデータのみ抽出して、Resume.owner_idとUsers.nameを紐付ける。JOINでデータ取った方が早いかも。
     @resumes = params[:language_id].present? ? Resume.where(language_id: params[:language_id]) : Resume.all
   end
 end
