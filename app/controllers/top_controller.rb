8870266b0e801d9dae861ae961a4a759f8ab0e2c
Hisafumi Kikkawa <a13z3hk@aiit.ac.jp>
2015-05-02 12:02:27 +0900

    fixed bug

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,10 +2,10 @@ class TopController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    lang = params[:language].blank? ? [] : params[:language]
+    lang = params[:language].blank? ? {} : params[:language]
     @resumes = lang.blank? ? 
         Resume.where.not(owner_id: current_user.id) :
         Resume.where(language_id: lang[:id]).where.not(owner_id: current_user.id)
-    @selected_lang = lang[:id].blank? ? '' : lang[:id]
+    @selected_lang = lang[:id]
   end
 end
