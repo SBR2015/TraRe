686de7c8f11d5d3cf31a706abfb0cec476c0d552
Hisafumi Kikkawa <a13z3hk@aiit.ac.jp>
2015-05-02 11:34:37 +0900

    modify SQL code

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,18 +3,7 @@ class TopController < ApplicationController
 
   def index
     lang = params[:language].blank? ? [] : params[:language]
-    resumes = lang.blank? ? Resume.all : Resume.where(language_id: lang[:id])
+    @resumes = lang.blank? ? Resume.all : Resume.where(language_id: lang[:id]).where.not(owner_id: current_user.id)
     @selected_lang = lang[:id]
-
-    @user_list = []
-    resumes.each do | resume |
-      if current_user.id != resume.owner_id
-        User.all.each do | user |
-          if user.id == resume.owner_id
-            @user_list.push user
-          end
-        end
-      end
-    end
   end
 end
