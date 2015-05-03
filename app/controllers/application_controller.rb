62700c7dcad65853c5ad7712c24aeb308dd89e69
Yusuke Ohashi <yusuke@junkpiano.me>
2015-05-03 17:17:23 +0900

    add languages(ja, en, vi, zh-CN, ko, fr).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   # device controller calls below methods
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_locale
 
   protected
 
@@ -29,8 +30,11 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    # set locale to browser session.
-    session[:locale] = params[:locale] if params[:locale]
-    I18n.locale = session[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    locale = I18n.locale unless I18n.locale == :en
+    options.merge locale: locale
   end
 end
